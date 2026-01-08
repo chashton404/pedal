ce9bb8bcf7c2b48efd421ab198f3f47da6d9ecee
Chase Ashton <chashton404@gmail.com>
2026-01-07 19:47:42 -0700

    Added lapcount to the HUD

--- src/misc/CoordinateHUD.jsx
+++ src/misc/CoordinateHUD.jsx
@@ -2,11 +2,12 @@ import { useGameStore } from "../store";
 
 export const CoordinateHUD = () => {
   const pos = useGameStore((s) => s.playerPosition);
+  const lap = useGameStore((state) => state.lapCount);
   const showCoordinates = useGameStore((s) => s.showCoordinates);
   if (!pos || !showCoordinates) return null;
   return (
     <div className="coords">
-      x: {pos.x.toFixed(2)} y: {pos.y.toFixed(2)} z: {pos.z.toFixed(2)}
+      x: {pos.x.toFixed(2)} y: {pos.y.toFixed(2)} z: {pos.z.toFixed(2)} Lap: {lap}
     </div>
   );
 };
